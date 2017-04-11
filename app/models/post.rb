352ca3007f5bcc8b71419168aa326b74ff30de71
szrharrison <s.z.r.harrison@gmail.com>
2017-04-11 17:40:40 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
+class MyValidator < ActiveModel::Validator
+  def validate(post)
+    unless post.title =~ /Won't Believe|Secret|Guess|Top \d/
+      post.errors[:title] << "This title isn't clickbait-y enough!"
+    end
+  end
+end
+
+
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+  include ActiveModel::Validations
+  validates_with MyValidator
 end
